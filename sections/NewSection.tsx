3d84b279717bcf86553000407efa0426d389213c
Toshi <toshii2310@gmail.com>
2024-10-29 14:38:41 +0000

    Publish 5 from staging into origin/main

---
+++ sections/NewSection.tsx
@@ -0,0 +1,10 @@
+interface Props {
+  /**
+  * @description The description of name.
+  */
+  name?: string;
+}
+
+export default function Section({ FAMILÍA = "Capy" }: Props) {
+  return <div>SALVE {name}</div>
+}
